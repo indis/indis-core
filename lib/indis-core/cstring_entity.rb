a96ac67c8647b2aec1bb26c22ecbc023097b9b82
Vladimir Pouzanov <farcaller@gmail.com>
2012-04-19 21:38:08 +0300

    Added CStingEntity that represents a C (zero-terminated) string

---
+++ lib/indis-core/cstring_entity.rb
@@ -0,0 +1,47 @@
+##############################################################################
+#   Indis framework                                                          #
+#   Copyright (C) 2012 Vladimir "Farcaller" Pouzanov <farcaller@gmail.com>   #
+#                                                                            #
+#   This program is free software: you can redistribute it and/or modify     #
+#   it under the terms of the GNU General Public License as published by     #
+#   the Free Software Foundation, either version 3 of the License, or        #
+#   (at your option) any later version.                                      #
+#                                                                            #
+#   This program is distributed in the hope that it will be useful,          #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#   GNU General Public License for more details.                             #
+#                                                                            #
+#   You should have received a copy of the GNU General Public License        #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
+##############################################################################
+
+require 'indis-core/entity'
+
+module Indis
+  
+  class CStringEntity < Entity
+    attr_reader :value # @return [String] the value of entity
+    
+    # @param [Fixnum] ofs virtual address
+    # @param [Indus::VMMap] vmmap map of the target to load value from
+    def initialize(ofs, vmmap)
+      super ofs
+      
+      @value = ''
+      addr = ofs
+      begin
+        c = vmmap.byte_at(addr)
+        @value += c.chr
+        addr += 1
+      end while c != 0
+      
+      @size = @value.length
+    end
+    
+    def to_s
+      "BYTES\t#{@value[0...-1]}"
+    end
+  end
+  
+end
---
+++ spec/indis-core/cstring_entity_spec.rb
@@ -0,0 +1,12 @@
+require 'indis-core/cstring_entity'
+
+describe Indis::CStringEntity do
+  it "should load its value from vmmap" do
+    str = "hello\0world".unpack('C*')
+    map = double('VMMap')
+    map.should_receive(:byte_at) { |i| str[i] }.exactly(6).times
+    e = Indis::CStringEntity.new(0, map)
+    e.to_s.should == "BYTES\thello"
+    e.value.should == "hello\x0"
+  end
+end
